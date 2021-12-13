9e9dc73046e2be0f7fda01a95cb827acc3a98434
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2021-12-13 18:31:37 +0200

    Build

--- src/database/index.js
+++ src/database/index.js
@@ -6,19 +6,6 @@ const { Sequelize } = require('sequelize');
 //     dialect: 'mysql'
 // });
 
-const sequelize = new Sequelize('de20kjd3gtflj5', 'fprsqkzcofhrdn', '15d3d0a200a4cbee7e0693f087e91c475a3c76b631e99fe93015bd53e4638a17', {
-    repositoryMode: true,
-    port: 5432,
-    host: 'ec2-18-210-159-154.compute-1.amazonaws.com',
-    dialect: 'postgres',
-    "dialectOption":{
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-});
-
 
 const sequelize = new Sequelize('de20kjd3gtflj5', 'fprsqkzcofhrdn', '15d3d0a200a4cbee7e0693f087e91c475a3c76b631e99fe93015bd53e4638a17', {
     repositoryMode: true,
