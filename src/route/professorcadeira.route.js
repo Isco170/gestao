6c90356b1537dfcf7047faf54df4dca1d0dbdfcd
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2022-01-15 21:01:08 +0200

    adjustments

--- src/route/professorcadeira.route.js
+++ src/route/professorcadeira.route.js
@@ -3,5 +3,7 @@ let router = express.Router();
 const profCadeira = require('../controller/professorcadeira.controller');
 
 router.post('/profcadeira', profCadeira.addCadeira);
+router.get('/profcadeira/:usuario_id', profCadeira.readCadeiras);
+router.delete('/profcadeira', profCadeira.removeCadeira);
 
 module.exports = router;
