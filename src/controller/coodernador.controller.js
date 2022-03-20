c4757e10d3c216983a58f5ad16fde09a77c80420
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2022-03-20 16:42:34 +0200

    Listar

--- src/controller/coodernador.controller.js
+++ src/controller/coodernador.controller.js
@@ -1,12 +1,19 @@
 const usuario = require('../model/usuario.model');
 
+function filtrar(usuario){
+    return usuario.role != 1;
+}
+
 async function read(request, response){
     try {
         const cord = await usuario.findAll();
+        
+        let lista = cord.filter(filtrar)
+
         return response.status(202).json({
             error: false,
             message: 'Lista de coordenadores',
-            data: cord
+            data: lista
         })
     } catch (error) {
         return response.status(500).json({
