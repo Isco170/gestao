1043576189eed50de8c4ef1186724afb68155680
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2022-07-12 20:31:50 +0200

    Deploy

--- src/database/index.js
+++ src/database/index.js
@@ -1,24 +1,24 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('gestao', 'root', '', {
-    repositoryMode: true,
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-
-// const sequelize = new Sequelize('du2etqic4fb1u', 'raneyhkhptuchc', 'c5cff6e4b31009d529c8cc07e17280b615746d966615f855831398382932906f', {
+// const sequelize = new Sequelize('gestao', 'root', '', {
 //     repositoryMode: true,
-//     port: 5432,
-//     host: 'ec2-34-200-35-222.compute-1.amazonaws.com',
-//     dialect: "postgres",
-//     "dialectOptions": {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false
-//         }
-//     }
+//     host: 'localhost',
+//     dialect: 'mysql'
 // });
+
+
+const sequelize = new Sequelize('du2etqic4fb1u', 'raneyhkhptuchc', 'c5cff6e4b31009d529c8cc07e17280b615746d966615f855831398382932906f', {
+    repositoryMode: true,
+    port: 5432,
+    host: 'ec2-34-200-35-222.compute-1.amazonaws.com',
+    dialect: "postgres",
+    "dialectOptions": {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
 
 
 
